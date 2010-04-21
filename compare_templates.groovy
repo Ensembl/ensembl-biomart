4260ecc05af1e6b3adf1c83737ee7caa6c2f7b46
dstaines <>
2010-04-21 08:34:49 +0000

    rewrote using XmlParser to allow merging of templates together

--- compare_templates.groovy
+++ compare_templates.groovy
@@ -1,15 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import groovy.xml.StreamingMarkupBuilder 
+import groovy.xml.XmlUtil;
+
 
 def compareTemplates(def source, def target) {
-	def sourceConf = new XmlSlurper().parse(source)
-	def targetConf = new XmlSlurper().parse(target)
+	def sourceConf = new XmlParser().parse(source)
+	def targetConf = new XmlParser().parse(target)
 	compareNodes sourceConf, targetConf, "AttributePage"
 	compareNodes sourceConf, targetConf, "FilterPage"
+	return sourceConf
+}
+
+def isBlackListed(def name) {
+	def found = false
+	["new_attributes","new_filters",".*paralog.*",".*homolog.*",".*ortholog.*","multispecies","new_filters"].each {
+		if(name.toString().toLowerCase().matches(it)) {
+			found = true
+		}
+	}           
+	return found
 }
 
 def compareNodes(def src, def target, def childName) {
-	def filters = ["new_attributes","new_filters"]
-	//println src.name +"/"+src.attributes.internalName+" <-> "+src.name +"/"+src.attributes.internalName+" -> "+childName
-	if(!filters.contains(src.attributes.internalName)) {
+	if(!isBlackListed(src.attributes().internalName)) {
 		compareNodeByAttributes(src,target)
 		if(childName) {
 			def relations = [
@@ -25,7 +40,7 @@ def compareNodes(def src, def target, def childName) {
 			def srcChildren = hashByName(src,childName)
 			def targetChildren = hashByName(target,childName)
 			srcChildren.each { 
-				def targetChild = targetChildren.get(it.key)
+				def targetChild = targetChildren.get(it.key.toLowerCase())
 				def srcChild = it.value
 				if(targetChild) {
 					compareNodes srcChild,targetChild, grandChild 
@@ -34,8 +49,16 @@ def compareNodes(def src, def target, def childName) {
 				}
 			}
 			targetChildren.each { 
-				if(!srcChildren.get(it.key)) {
-					println "NEW: "+childName+"/"+it.key				
+				if(!srcChildren.get(it.key.toLowerCase()) && !isBlackListed(it.key)) {
+					println "NEW: "+it.value.name()+"/"+it.key
+					def newNode = new Node(src,
+							it.value.name(),
+				            it.value.attributes())
+//					if(src.children.getClass().name == 'groovy.util.slurpersupport.NodeChildren') {
+						src.appendNode(newNode)						
+//					} else {
+//						src.children.add(it.value)
+//					}
 				}
 			}
 		}
@@ -43,40 +66,42 @@ def compareNodes(def src, def target, def childName) {
 }
 
 def compareNodeByAttributes(def srcNode, def targetNode) {
-	srcNode.attributes.each {
-		def targVal = targetNode.attributes.get(it.key)		
+	srcNode.attributes().each {
+		def targVal = targetNode.attributes().get(it.key)		
 		if(targVal != it.value) {
-			println "DIFF: "+srcNode.name+"/"+srcNode.attributes.internalName+": "+it.value+" <=> "+targVal
+			println "DIFF: "+srcNode.name()+"/"+srcNode.attributes().internalName+": "+it.value+" <=> "+targVal
+			srcNode.attributes().put(it.key,it.value)
 		}
 	}
 }
 
 def hashByName(def conf, def name) {
-	def hash = [:]	            
-	conf.findAll {
-		it.children.findAll { it.name == name }.each {			
-			hash.(it.attributes.internalName) = it 
+	def hash = [:]	
+	conf.depthFirst().grep{ it.getClass().name == 'groovy.util.Node' && it.name() == name }.each {			
+			hash.(it.attributes().internalName.toLowerCase()) = it 
 		}
-	}
 	return hash;
 }
 
 def cli = new CliBuilder(usage: 'compare_templates.groovy -source src_xml -target target_xml')
 cli.with {
-h longOpt: 'help', 'Show usage information'
-s longOpt: 'source', args: 1, argName: 'source', 'File containing source template'
-t longOpt: 'target', args: 1, argName: 'target', 'File containing target template'
+	h longOpt: 'help', 'Show usage information'
+	s longOpt: 'source', args: 1, argName: 'source', 'File containing source template'
+	t longOpt: 'target', args: 1, argName: 'target', 'File containing target template'
+	m longOpt:'merge', 'Merge the changes into a new file'
 }
- 
+
 def options = cli.parse(args)
 if (!options) {
-System.exit 1
+	System.exit 1
 }
 if (options.h) {
-cli.usage()
-System.exit 0
+	cli.usage()
+	System.exit 0
 }
 
-compareTemplates options.s,options.t
-
-//"/home/dstaines/biomart/ensembl_mart/e55_template.xml","/home/dstaines/biomart/ensembl_mart/e57_template.xml"
+def merged = compareTemplates(options.s,options.t)
+if(options.m) {
+	new XmlNodePrinter(new PrintWriter(new FileWriter(options.s+".merged"))).print(merged)
+	new XmlNodePrinter(new PrintWriter(new FileWriter(options.s+".original"))).print(new XmlParser().parse(options.s))
+}
