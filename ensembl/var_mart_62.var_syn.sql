a5554e0cb211b83b6ca66b2bab51be8b4625f3d7
Dan Staines <dstaines@ebi.ac.uk>
2014-01-13 13:00:23 +0000

    updated licenses

--- ensembl/var_mart_62.var_syn.sql
+++ ensembl/var_mart_62.var_syn.sql
@@ -1,3 +1,17 @@
+-- Copyright [2009-2014] EMBL-European Bioinformatics Institute
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--      http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 create table VAR_MART_DB.TEMP7 as select a.source_id as source_id_2021,a.description as description_2021,a.name as name_2021,a.version as version_2021,a.type as type_2021 from VAR_DB.source as a where a.source_id=SRC_ID;
 create index I_11 on VAR_MART_DB.TEMP7(source_id_2021);
 create table VAR_MART_DB.TEMP8 as select a.*,b.subsnp_id as subsnp_id_2030,b.name as name_2030,b.moltype as moltype_2030,b.variation_id as variation_id_2025_key,b.variation_synonym_id as variation_synonym_id_2030 from VAR_MART_DB.TEMP7 as a left join VAR_DB.variation_synonym as b on a.source_id_2021=b.source_id where VAR_ID_COND_b
--- ensembl/var_mart_67.var.sql
+++ ensembl/var_mart_67.var.sql
@@ -1,3 +1,17 @@
+-- Copyright [2009-2014] EMBL-European Bioinformatics Institute
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--      http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 create table VAR_MART_DB.TEMP0 as select a.validation_status as validation_status_2025,a.source_id as source_id_2025,a.ancestral_allele as ancestral_allele_2025,a.name as name_2025,a.class_attrib_id as class_attrib_id_2025,a.variation_id as variation_id_2025_key,a.somatic as somatic_2025 from VAR_DB.variation as a where VAR_ID_COND_a
 create index I_0 on VAR_MART_DB.TEMP0(source_id_2025);
 create table VAR_MART_DB.TEMP1 as select a.*,b.description as description_2021,b.name as name_2021,b.version as version_2021,b.type as type_2021 from VAR_MART_DB.TEMP0 as a left join VAR_DB.source as b on a.source_id_2025=b.source_id;
