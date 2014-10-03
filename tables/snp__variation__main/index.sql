0693d823de32aa533f84d22aec1b54429816af50
James Allen <jallen@ebi.ac.uk>
2014-10-03 15:13:08 +0100

    Can't create indexes on a column until the column exists...

--- tables/snp__variation__main/index.sql
+++ tables/snp__variation__main/index.sql
@@ -3,6 +3,3 @@ CREATE INDEX idx_2 ON SPECIES_ABBREV_snp__variation__main (name_2025);
 CREATE INDEX idx_3 ON SPECIES_ABBREV_snp__variation__main (name_2021);
 CREATE INDEX idx_4 ON SPECIES_ABBREV_snp__variation__main (clinical_significance_2025);
 CREATE INDEX idx_5 ON SPECIES_ABBREV_snp__variation__main (evidence_2025);
-CREATE INDEX idx_6 ON SPECIES_ABBREV_snp__variation__main (variation_annotation_bool);
-CREATE INDEX idx_7 ON SPECIES_ABBREV_snp__variation__main (variation_citation_bool);
-CREATE INDEX idx_8 ON SPECIES_ABBREV_snp__variation__main (variation_feature_count);
