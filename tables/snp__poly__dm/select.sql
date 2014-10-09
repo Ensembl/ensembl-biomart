6cf771ddacbab7d9a4487460446e9d32e11cc3d8
James Allen <jallen@ebi.ac.uk>
2014-10-09 11:35:24 +0100

    Generate the MTMP_sample_genotype table, formerly known as the tmp_individual_genotype_single_bp table

--- tables/snp__poly__dm/select.sql
+++ tables/snp__poly__dm/select.sql
@@ -1,10 +1,10 @@
 SELECT DISTINCT
   v.variation_id as variation_id_2025_key,
   i.name as name_2019,
-  concat(igs.allele_1, "|", igs.allele_2) as allele
+  concat(sg.allele_1, "|", sg.allele_2) as allele
 FROM
-  VAR_DB.tmp_individual_genotype_single_bp igs INNER JOIN
+  VAR_DB.MTMP_sample_genotype sg INNER JOIN
   VAR_DB.individual i ON
-    igs.individual_id = i.individual_id AND
+    sg.individual_id = i.individual_id AND
     i.display IN ("REFERENCE","DEFAULT","DISPLAYABLE","MARTDISPLAYABLE") RIGHT OUTER JOIN
-  VAR_DB.variation v ON igs.variation_id = v.variation_id
+  VAR_DB.variation v ON sg.variation_id = v.variation_id
