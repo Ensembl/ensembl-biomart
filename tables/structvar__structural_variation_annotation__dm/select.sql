9cf8e6cea0491837dc30974f782ef29e2e92fb5c
James Allen <jallen@ebi.ac.uk>
2015-08-03 16:16:04 +0100

    Accommodate switch from 'individual' to 'sample' in var_mart build pipeline

--- tables/structvar__structural_variation_annotation__dm/select.sql
+++ tables/structvar__structural_variation_annotation__dm/select.sql
@@ -1,8 +1,8 @@
 SELECT
   sv.structural_variation_id as structural_variation_id_2072_key,
-  REPLACE(i.name, ",", "") as name_2019,
-  i.description as description_2019
+  REPLACE(s.name, ",", "") as name_2019,
+  s.description as description_2019
 FROM
   VAR_DB.structural_variation sv LEFT OUTER JOIN
   VAR_DB.structural_variation_sample svs ON sv.structural_variation_id = svs.structural_variation_id LEFT OUTER JOIN
-  VAR_DB.individual i ON svs.strain_id = i.individual_id
+  VAR_DB.sample s ON svs.sample_id = s.sample_id
