18f8d27106afffada5af9d13cc71f379d415af88
Thomas Maurel <maurel@ebi.ac.uk>
2015-10-28 12:02:51 +0000

    Removing np__strain_gtype_poly__dm

--- tables/snp__strain_gtype_poly__dm/select.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-  v.variation_id as variation_id_2025_key,
-  REPLACE(sgp.sample_name, ",", "") as sample_name_2085
-FROM
-  VAR_DB.strain_gtype_poly sgp RIGHT OUTER JOIN
-  VAR_DB.variation v ON sgp.variation_id = v.variation_id
