9ef03b790e9badf3e19e8e8a0a1b270e6affdb28
Thomas Maurel <maurel@ebi.ac.uk>
2015-11-06 11:21:55 +0000

    Fixed bug introduce by previous commit, was only returning sample or population when one of them was 0 as a result the other was undef

--- Bio/EnsEMBL/EGPipeline/VariationMart/CopyOrGenerate.pm
+++ Bio/EnsEMBL/EGPipeline/VariationMart/CopyOrGenerate.pm
@@ -133,17 +133,22 @@ sub data_display {
       $show_sams = $sams ? 1 : 0;
       $show_pops = $pops ? 1 : 0;
       $self->warning("Genotypes never skipped for $species");
-    } else {
-      if ($sams == 0) {
-        $show_sams = 0;
-      }
-      elsif ($pops == 0) {
-        $show_pops = 0;
-      }
-      else{
-        $show_sams = $sams <= $sam_threshold ? 1 : 0;
-        $show_pops = $pops <= $pop_threshold ? 1 : 0;
-      }
+    } 
+    elsif ($sams <= 0 and $pops <= 0) {
+      $show_sams = 0;
+      $show_pops = 0;
+    }
+    elsif ($sams <= 0 and $pops >= 0) {
+      $show_sams = 0;
+      $show_pops = $pops <= $pop_threshold ? 1 : 0;     
+    }
+    elsif ($sams >= 0 and $pops <= 0) {
+      $show_sams = $sams <= $sam_threshold ? 1 : 0;
+      $show_pops = 0;
+    } 
+    else{
+      $show_sams = $sams <= $sam_threshold ? 1 : 0;
+      $show_pops = $pops <= $pop_threshold ? 1 : 0;
     }
   }
   
