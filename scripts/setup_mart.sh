4606d3af9db52ac26e82d80ce061abf775e249d5
Dan Staines <dstaines@ebi.ac.uk>
2016-09-30 17:17:37 +0100

    first pass at a script to setup for building a mart

---
+++ scripts/setup_mart.sh
@@ -0,0 +1,48 @@
+#!/bin/bash --
+
+srv=$1
+mart=$2
+
+base_dir=$(dirname $0)
+
+echo "Creating mart $mart on $srv"
+$srv -e "DROP DATABASE IF EXISTS $mart; CREATE DATABASE $mart"
+
+echo "Creating master schemata"
+
+ENSEMBL_VERSION=$(perl -e "use Bio::EnsEMBL::ApiVersion qw/software_version/; print software_version()")
+ 
+CORE=master_schema_$ENSEMBL_VERSION
+FUNCGEN=master_schema_funcgen_$ENSEMBL_VERSION
+VARIATION=master_schema_variation_$ENSEMBL_VERSION
+$srv -e "CREATE DATABASE IF NOT EXISTS $CORE;"
+$srv -e "CREATE DATABASE IF NOT EXISTS $FUNCGEN;"
+$srv -e "CREATE DATABASE IF NOT EXISTS $VARIATION;"
+
+$srv $CORE < $ENSEMBL_CVS_ROOT_DIR/ensembl/sql/table.sql
+$srv $FUNCGEN < $ENSEMBL_CVS_ROOT_DIR/ensembl-funcgen/sql/efg.sql
+$srv $VARIATION < $ENSEMBL_CVS_ROOT_DIR/ensembl-variation/sql/table.sql
+$srv $FUNCGEN < $HELPER_SQL
+
+echo "Creating funcgen helpers"
+HELPER_SQL=$base_dir/probestuff_helper.sql
+$srv -e "show databases" | grep funcgen | while read db; do
+$srv $db < $HELPER_SQL
+done
+
+echo "Creating variation helper for "
+HELPER_SQL=$base_dir/probestuff_helper.sql
+$srv -e "show databases" | grep funcgen | while read db; do
+$srv $db < $HELPER_SQL
+done
+
+echo "Creating variation helper for all dbs"
+cd ${ENSEMBL_CVS_ROOT_DIR}/ensembl-variation
+perl -I modules -I scripts/import \
+    scripts/misc/mart_variation_effect.pl \
+    $($srv details naga) \
+    -tmpdir /tmp -tmpfile mtmp.txt \
+    -table transcript_variation
+cd -
+
+echo "Completed setup of $mart on $srv"
