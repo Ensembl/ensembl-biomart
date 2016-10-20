a6b045480dbee9954a8cfb1a3e8dd364daba9fa4
Thomas Maurel <maurel@ebi.ac.uk>
2016-10-20 16:42:18 +0100

    Added an index on pub_short_ref as it's used as a filter

--- var_mart/tables/snp__variation_citation__dm/index.sql
+++ var_mart/tables/snp__variation_citation__dm/index.sql
@@ -1,1 +1,1 @@
-ALTER TABLE SPECIES_ABBREV_snp__variation_citation__dm ADD INDEX (variation_id_2025_key);
+ALTER TABLE SPECIES_ABBREV_snp__variation_citation__dm ADD INDEX (variation_id_2025_key), ADD INDEX (pub_short_ref);
