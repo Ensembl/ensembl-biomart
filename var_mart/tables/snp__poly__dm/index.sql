bdb08605694b0f70846ec4c231eeedd811916f66
Thomas Maurel <maurel@ebi.ac.uk>
2016-10-20 16:38:43 +0100

    Moving variation mart pipeline SQLs and indexes into var_mart directory to follow eg-biomart structure

---
+++ var_mart/tables/snp__poly__dm/index.sql
@@ -0,0 +1,2 @@
+CREATE INDEX idx_1 ON SPECIES_ABBREV_snp__poly__dm (variation_id_2025_key);
+CREATE INDEX idx_2 ON SPECIES_ABBREV_snp__poly__dm (name_2019);
---
+++ var_mart/tables/snp__regulatory_feature_variation__dm/select.sql
@@ -0,0 +1,8 @@
+SELECT
+  vf.variation_feature_id as variation_feature_id_2026_key,
+  rfv.feature_stable_id as feature_stable_id_20126,
+  rfv.allele_string as allele_string_20126,
+  rfv.consequence_types as consequence_types_20126
+FROM
+  VAR_DB.MTMP_regulatory_feature_variation rfv RIGHT OUTER JOIN
+  VAR_DB.variation_feature vf ON rfv.variation_feature_id = vf.variation_feature_id
