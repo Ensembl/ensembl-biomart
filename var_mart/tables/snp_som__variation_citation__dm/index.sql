7fd345229de2ebec05b53db32634b81309db229f
Thomas Maurel <maurel@ebi.ac.uk>
2016-10-21 09:42:22 +0100

    Added pub_ref column in the snp_som__variation_citation__dm table. Added an index on this column

--- var_mart/tables/snp_som__variation_citation__dm/index.sql
+++ var_mart/tables/snp_som__variation_citation__dm/index.sql
@@ -1,1 +1,1 @@
-ALTER TABLE SPECIES_ABBREV_snp_som__variation_citation__dm ADD INDEX (variation_id_2025_key);
+ALTER TABLE SPECIES_ABBREV_snp_som__variation_citation__dm ADD INDEX (variation_id_2025_key), ADD INDEX (pub_short_ref);
