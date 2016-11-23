0dde109a77eb2e772d6fae19f42a84e1c313200f
Amonida Zadissa <amonida@ebi.ac.uk>
2016-11-23 09:43:54 +0000

    Specify the correct path for env.

--- scripts/add_compara.pl
+++ scripts/add_compara.pl
@@ -1,4 +1,4 @@
-#!/bin/env perl
+#!/usr/bin/env perl
 # Copyright [2009-2014] EMBL-European Bioinformatics Institute
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -42,6 +42,7 @@ my $compara_db;
 my $dataset_name;
 my $limit_species;
 my $basename='gene';
+my $template;
 
 my $template_dir = "$FindBin::Bin/templates";
 
