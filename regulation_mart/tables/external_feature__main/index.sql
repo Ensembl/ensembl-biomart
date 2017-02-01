11530c32d4e3d503f290a1188e4b5dcd78b3400b
Thomas Maurel <maurel@ebi.ac.uk>
2017-02-01 11:04:53 +0000

    New hive to run generate_names, add compound index seq_region_name, seq_region_start and seq_region_end for all the templates, tidy tables, optimize and generate_meta.

---
+++ regulation_mart/tables/external_feature__main/index.sql
@@ -0,0 +1,1 @@
+ALTER TABLE SPECIES_ABBREV__external_feature__main ADD INDEX seq_region_idx (seq_region_name_1049, seq_region_start_1049, seq_region_end_1049);
---
+++ regulation_mart/tables/motif_feature__main/index.sql
@@ -0,0 +1,1 @@
+ALTER TABLE SPECIES_ABBREV__motif_feature__main ADD INDEX seq_region_idx (seq_region_name_1075, seq_region_start_1075, seq_region_end_1075);
