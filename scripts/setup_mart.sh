4cd9a961c396aeb78cc705c10e281796e2e4dcfb
Thomas Maurel <maurel@ebi.ac.uk>
2017-03-10 10:46:34 +0000

    Replacing ENSEMBL_CVS_ROOT_DIR with ensembl production BASE_DIR variable for consistency

--- scripts/setup_mart.sh
+++ scripts/setup_mart.sh
@@ -4,8 +4,6 @@
 srv=$1
 mart=$2
 
-base_dir=$(dirname $0)
-
 echo "Creating mart $mart on $srv"
 $srv -e "DROP DATABASE IF EXISTS $mart; CREATE DATABASE $mart"
 
@@ -16,7 +14,7 @@ ENSEMBL_VERSION=$(perl -e "use Bio::EnsEMBL::ApiVersion qw/software_version/; pr
 CORE=master_schema_$ENSEMBL_VERSION
 FUNCGEN=master_schema_funcgen_$ENSEMBL_VERSION
 VARIATION=master_schema_variation_$ENSEMBL_VERSION
-HELPER_SQL=$base_dir/probestuff_helper.sql
+HELPER_SQL=$BASE_DIR/ensembl-biomart/scripts/probestuff_helper.sql
 
 function count_database {
     $srv --column-names=false -e "show databases" | grep $1 | wc -l
@@ -25,19 +23,19 @@ function count_database {
 if [ $(count_database $CORE) -eq 0 ]; then
     echo "Creating $CORE"
     $srv -e "CREATE DATABASE $CORE"
-    $srv $CORE < $ENSEMBL_CVS_ROOT_DIR/ensembl/sql/table.sql
+    $srv $CORE < $BASE_DIR/ensembl/sql/table.sql
 fi
     
 if [ $(count_database $FUNCGEN) -eq 0 ]; then
     echo "Creating $FUNCGEN"
     $srv -e "CREATE DATABASE $FUNCGEN"
-    $srv $FUNCGEN < $ENSEMBL_CVS_ROOT_DIR/ensembl-funcgen/sql/table.sql
+    $srv $FUNCGEN < $BASE_DIR/ensembl-funcgen/sql/table.sql
 fi
 
 if [ $(count_database $VARIATION) -eq 0 ]; then
     echo "Creating $VARIATION"
     $srv -e "CREATE DATABASE $VARIATION;"
-    $srv $VARIATION < $ENSEMBL_CVS_ROOT_DIR/ensembl-variation/sql/table.sql
+    $srv $VARIATION < $BASE_DIR/ensembl-variation/sql/table.sql
 fi
 
 $srv -e "show databases" | grep funcgen | while read db; do
@@ -52,7 +50,7 @@ $srv -e "show databases" | grep variation | while read db; do
     cnt=$($srv --column-names=false $db -e "show tables like \"MTMP_transcript_variation\"" | wc -l)
     if [ $cnt -eq 0 ]; then 
     echo "Creating variation transcript_variation MTMP table for $db"
-    cd ${ENSEMBL_CVS_ROOT_DIR}/ensembl-variation
+    cd ${BASE_DIR}/ensembl-variation
     perl -I modules -I scripts/import \
         scripts/misc/mart_variation_effect.pl \
         $($srv details script) \
@@ -66,7 +64,7 @@ $srv -e "show databases" | grep variation | while read db; do
     cnt=$($srv --column-names=false $db -e "show tables like \"MTMP_phenotype\"" | wc -l)
     if [ $cnt -eq 0 ]; then
 	echo "Creating variation phenotype MTMP table for $db"
-	cd ${ENSEMBL_CVS_ROOT_DIR}/ensembl-variation
+	cd ${BASE_DIR}/ensembl-variation
 	perl -I modules -I scripts/import \
 	    scripts/misc/mart_phenotypes.pl \
 	    $($srv details script) \
