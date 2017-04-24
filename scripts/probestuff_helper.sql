3a4744557172a2320231661b83d391a60688c95d
Thomas Maurel <maurel@ebi.ac.uk>
2017-04-24 14:14:27 +0100

    Added exception for Rabbit

--- scripts/probestuff_helper.sql
+++ scripts/probestuff_helper.sql
@@ -41,4 +41,8 @@ ALTER TABLE MTMP_probestuff_helper
   ADD INDEX transcript_idx (transcript_stable_id),
   ADD INDEX vendor_name_idx (array_vendor_and_name);
 
+-- The folowing probe name was to long for Rabbit and the name was exceding the mysql limit of 64 characters in biomart: ocuniculus_gene_ensembl__eFG_AGILENT_SurePrint_GPL16709_4x44k__dm
+
+UPDATE MTMP_probestuff_helper SET array_vendor_and_name ='AGILENT_SurePrnt_GPL16709_4x44k' WHERE array_vendor_and_name='AGILENT_SurePrint_GPL16709_4x44k';
+
 OPTIMIZE TABLE MTMP_probestuff_helper;
