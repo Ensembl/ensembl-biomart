c5310eaacbaf187bce0e1488c2946d9ff205c107
Thomas Maurel <maurel@ebi.ac.uk>
2017-07-27 13:28:32 +0100

    Added subroutine to drop column if exist. Added subroutine to replicate the column from main table to child table, e.g: from gene to transcript and translation. This is needed as BioMart requires all the columns from gene into transcript and translation for optimised query

--- modules/Bio/EnsEMBL/BioMart/ConcatColumns.pm
+++ modules/Bio/EnsEMBL/BioMart/ConcatColumns.pm
@@ -47,6 +47,8 @@ sub create_stable_id_version_column {
 
   if (defined $column1_values->[0] and defined $column2_values->[0]) {
     if ($column1_values->[0] > 0 and $column2_values->[0] > 0) {
+      #Drop column if exist
+      $self->drop_column_if_exist($mart_dbc,$mart_table, $table."_stable_id_version");
 
       # Get first column type
       my $column1_types = $mart_dbc->sql_helper->execute_simple(-SQL=>"SELECT COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '$mart_table' AND COLUMN_NAME = '$column1';"); 
@@ -57,10 +59,47 @@ sub create_stable_id_version_column {
       $mart_dbc->sql_helper->execute_update(-SQL=>"UPDATE $mart_table SET ${table}_stable_id_version = CONCAT($column1, '$concat_separator', $column2);");
       # Create index
       $mart_dbc->sql_helper->execute_update(-SQL=>"ALTER TABLE $mart_table ADD INDEX (${table}_stable_id_version);");
+      # Replicate column in child tables.
+      # E.g: from Gene main table to Transcript and Translation
+      if ($table =~ m/gene/i) {
+        # Add the column to the Transcript main table
+        $self->replicate_column_child_tables($mart_dbc,$mart_table,$mart_table_prefix,"transcript__main",'gene__main_stable_id_version','gene_id_1020_key',$column1_type,$column1,$column2,$concat_separator);
+        # Add the column to the Translation main table
+        $self->replicate_column_child_tables($mart_dbc,$mart_table,$mart_table_prefix,"translation__main",'gene__main_stable_id_version','gene_id_1020_key',$column1_type,$column1,$column2,$concat_separator);
+      }
+      elsif ($table =~ m/transcript/i) {
+        # Add the column to the Translation main table
+        $self->replicate_column_child_tables($mart_dbc,$mart_table,$mart_table_prefix,"translation__main",'transcript__main_stable_id_version','transcript_id_1064_key',$column1_type,$column1,$column2,$concat_separator);
+      }
     }
   }
 
   $mart_dbc->disconnect_if_idle();
+}
+
+sub drop_column_if_exist {
+  my ($self, $mart_dbc, $mart_table, $column) = @_;
+  #DROP column if it exist
+  my $column1_exist = $mart_dbc->sql_helper()->execute_simple( -SQL =>"SELECT count(COLUMN_NAME) FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '$mart_table' AND COLUMN_NAME = '$column';" );
+  if (defined $column1_exist->[0]) {
+    if ($column1_exist->[0] > 0) {
+      $mart_dbc->sql_helper->execute_update(-SQL=>"ALTER TABLE $mart_table DROP COLUMN $column;");
+    }
+  }
+}
+
+sub replicate_column_child_tables {
+  my ($self, $mart_dbc, $mart_table, $mart_table_prefix, $child_table_name, $column, $main_table_key, $column_type, $column1,$column2,$concat_separator) = @_;
+  # Add the column to the Transcript main table
+  my $child_mart_table = "$mart_table_prefix$child_table_name";
+  #Drop column if exist
+  $self->drop_column_if_exist($mart_dbc,$child_mart_table,$column);
+  # Create new column
+  $mart_dbc->sql_helper->execute_update(-SQL=>"ALTER TABLE $child_mart_table ADD COLUMN $column $column_type;");
+  # Concat table
+  $mart_dbc->sql_helper->execute_update(-SQL=>"UPDATE $child_mart_table INNER JOIN $mart_table ON $mart_table.$main_table_key = $child_mart_table.$main_table_key SET $child_mart_table.$column = CONCAT($mart_table.$column1, '$concat_separator', $mart_table.$column2);");
+  # Create index
+  $mart_dbc->sql_helper->execute_update(-SQL=>"ALTER TABLE $child_mart_table ADD INDEX ($column);");
 }
 
 1;
