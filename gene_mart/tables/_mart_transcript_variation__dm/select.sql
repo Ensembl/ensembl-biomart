6b73d7371a5a54685e9547f916ecc8b79e255e5a
Thomas Maurel <maurel@ebi.ac.uk>
2017-10-04 16:02:06 +0100

    Adding select and index sql files for the mart_transcript_variation table

---
+++ gene_mart/tables/_mart_transcript_variation__dm/select.sql
@@ -0,0 +1,34 @@
+SELECT
+  t.transcript_id as transcript_id_1064_key,
+  t.stable_id as stable_id_1066,
+  tv.cdna_start as cdna_start_2076,
+  tv.cds_start as cds_start_2076,
+  tv.cds_end as cds_end_2076,
+  tv.translation_start as translation_start_2076,
+  tv.consequence_types as consequence_types_2076,
+  tv.distance_to_transcript as distance_to_transcript_2076,
+  tv.allele_string as allele_string_2076,
+  tv.pep_allele_string as pep_allele_string_2076,
+  tv.polyphen_score as polyphen_score_2076,
+  tv.polyphen_prediction as polyphen_prediction_2076,
+  tv.sift_score as sift_score_2076,
+  tv.sift_prediction as sift_prediction_2076,
+  vf.allele_string as allele_string_2026,
+  vf.minor_allele_count as minor_allele_count_2025,
+  vf.minor_allele_freq as minor_allele_freq_2025,
+  vf.minor_allele as minor_allele_2025,
+  vf.seq_region_end as seq_region_end_2026,
+  vf.seq_region_start as seq_region_start_2026,
+  vf.seq_region_strand as seq_region_strand_2026,
+  vf.map_weight as map_weight_2026,
+  vf.variation_name as name_2025,
+  vf.clinical_significance as clinical_significance_2025,
+  s.description as description_2021,
+  s.name as name_2021,
+  e.evidence as evidence_2025
+FROM
+  VAR_DB.MTMP_transcript_variation tv INNER JOIN
+  VAR_DB.variation_feature vf ON tv.variation_feature_id = vf.variation_feature_id and (vf.somatic=0 and vf.display=1) INNER JOIN
+  VAR_DB.source s ON vf.source_id = s.source_id LEFT JOIN
+  VAR_DB.MTMP_evidence as e ON vf.variation_id = e.variation_id RIGHT JOIN
+  CORE_DB.transcript t ON t.stable_id = tv.feature_stable_id
---
+++ gene_mart/tables/_mart_transcript_variation_som__dm/index.sql
@@ -0,0 +1,1 @@
+ALTER TABLE SPECIES_ABBREV__mart_transcript_variation_som__dm ADD INDEX (transcript_id_1064_key), ADD INDEX (name_2021), ADD INDEX (consequence_types_2076), ADD INDEX (evidence_2025);
