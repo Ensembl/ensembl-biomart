2cc4cada1be2d4064f8a2bc7e9cfa361d7366ae4
Thomas Maurel <maurel@ebi.ac.uk>
2017-10-09 16:01:46 +0100

    Renamed index file from annotated_features to peaks

--- regulation_mart/tables/annotated_feature__main/index.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE SPECIES_ABBREV__annotated_feature__main ADD INDEX seq_region_idx (seq_region_name_1048, seq_region_start_1048, seq_region_end_1048 );
---
+++ regulation_mart/tables/peak__main/index.sql
@@ -0,0 +1,1 @@
+ALTER TABLE SPECIES_ABBREV__peak__main ADD INDEX seq_region_idx (seq_region_name_1048, seq_region_start_1048, seq_region_end_1048 );
