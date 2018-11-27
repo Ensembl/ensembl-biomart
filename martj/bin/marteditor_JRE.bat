05fb36fe02b9fb5b17504447ab598cb45a95afa7
Thomas Maurel <maurel@ebi.ac.uk>
2018-11-27 11:18:06 +0000

    Retiring old marj repository used for martconfiguration

--- martj/bin/marteditor_JRE.bat
+++
@@ -1,4 +0,0 @@
-REM Note: If you get Java "Out of memory" errors, try increasing the numbers
-REM in the -Xmx and -Xms parameters in the java command below. For performance
-REM sake it is best if they are both the same value.
-..\jre\windows\bin\java -Xmx128m -Xms128m -ea -cp ..\build\classes;..\lib\martj.jar;..\lib\log4j-1.2.6.jar;..\lib\ecp1_0beta.jar;..\lib\mysql-connector-java-3.1.14-bin.jar;..\lib\postgresql-8.3-604.jdbc3.jar;..\lib\ensj-util.jar;..\lib\jdom.jar;..\lib\ojdbc14.jar;.. org.ensembl.mart.editor.MartEditor
--- martj/data/builder/sql/final.sql
+++
@@ -1,8 +0,0 @@
-alter table kegg__gene__main change gene_id gene_id_key int(11);
-alter table kegg__gene__main add index(gene_id_key);
-alter table kegg__ec__dm change gene_id gene_id_key int(11);
-alter table kegg__ec__dm add index(gene_id_key);
-alter table kegg__pfam__dm change gene_id gene_id_key int(11);
-alter table kegg__pfam__dm add index(gene_id_key);
-alter table kegg__path__dm change gene_id gene_id_key int(11);
-alter table kegg__path__dm add index(gene_id_key);
