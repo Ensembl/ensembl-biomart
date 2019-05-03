220de2e65ea69a53e742e91515cadfd50474a317
Thomas Maurel <maurel@ebi.ac.uk>
2019-05-03 16:35:44 +0100

    The variation team have moved the ancestral_allele column from the variation to variation_feature table. Changes to make sure that we can still retrieve this data and updated template to use the new column

--- var_mart/tables/snp__variation_feature__main/select.sql
+++ var_mart/tables/snp__variation_feature__main/select.sql
@@ -1,7 +1,6 @@
 SELECT
   v_m.variation_id_2025_key,
   v_m.name_2025,
-  v_m.ancestral_allele_2025,
   v_m.minor_allele_2025,
   v_m.minor_allele_count_2025,
   v_m.minor_allele_freq_2025,
@@ -16,6 +15,7 @@ SELECT
   vf.seq_region_start as seq_region_start_2026,
   vf.seq_region_end as seq_region_end_2026,
   vf.seq_region_strand as seq_region_strand_2026,
+  vf.ancestral_allele as ancestral_allele_2025,
   vf.allele_string as allele_string_2026,
   vf.map_weight as map_weight_2026
 FROM
