c98b57f5389b85620128ad47fcca42416e1f2314
vinay-ebi <vinay@ebi.ac.uk>
2022-04-01 12:02:20 +0100

    base conf file to declare analysis flow for vert and nonvert variation mart MTMP table creation

--- modules/Bio/EnsEMBL/PipeConfig/BuildVariationMartMTMPTables_eg_conf.pm
+++ modules/Bio/EnsEMBL/PipeConfig/BuildVariationMartMTMPTables_eg_conf.pm
@@ -22,7 +22,7 @@ package Bio::EnsEMBL::PipeConfig::BuildVariationMartMTMPTables_eg_conf;
 use strict;
 use warnings;
 
-use base ('Bio::EnsEMBL::PipeConfig::VariationMart_conf');
+use base ('Bio::EnsEMBL::PipeConfig::BuildVariationMartMTMPTables_conf');
 
 sub default_options {
     my ($self) = @_;
@@ -63,68 +63,5 @@ sub default_options {
         },
     };
 }
-
-
-sub pipeline_analyses {
-    my ($self) = @_;
-    my $analyses = [
-        {
-            -logic_name      => 'InitialiseMartDB',
-            -module          => 'Bio::EnsEMBL::VariationMart::InitialiseMartDB',
-            -input_ids       => [ {} ],
-            -parameters      => {
-                mart_db_name => $self->o('mart_db_name'),
-                drop_mart_db => $self->o('drop_mart_db'),
-            },
-            -max_retry_count => 0,
-            -flow_into       => [ 'SpeciesFactory' ]
-        },
-        {
-            -logic_name      => 'SpeciesFactory',
-            -module          => 'Bio::EnsEMBL::Production::Pipeline::Common::SpeciesFactory',
-            -parameters      => {
-                species     => $self->o('species'),
-                antispecies => $self->o('antispecies'),
-                division    => $self->o('division'),
-                run_all     => $self->o('run_all'),
-            },
-            -max_retry_count => 0,
-            -flow_into       => {
-                '4' => 'DropMartTables',
-            }
-        },
-        {
-            -logic_name        => 'DropMartTables',
-            -module            => 'Bio::EnsEMBL::VariationMart::DropMartTables',
-            -parameters        => {
-                drop_mart_tables => $self->o('drop_mart_tables'),
-            },
-            -max_retry_count   => 0,
-            -analysis_capacity => 5,
-            -flow_into         => [ 'CreateMTMPTables' ],
-        },
-        {
-            -logic_name        => 'CreateMTMPTables',
-            -module            => 'Bio::EnsEMBL::VariationMart::CreateMTMPTables',
-            -parameters        => {
-                drop_mtmp                => $self->o('drop_mtmp'),
-                drop_mtmp_tv             => $self->o('drop_mtmp_tv'),
-                variation_import_lib     => $self->o('variation_import_lib'),
-                variation_feature_script => $self->o('variation_feature_script'),
-                variation_mtmp_script    => $self->o('variation_mtmp_script'),
-                registry                 => $self->o('registry'),
-                scratch_dir              => $self->o('scratch_dir'),
-            },
-            -max_retry_count   => 3,
-            -analysis_capacity => 5,
-            -can_be_empty      => 1,
-            -rc_name           => '16Gb_mem',
-        },
-    ];
-
-    return $analyses;
-}
-
-
 
 1;
