3ece00e2ede9d422f9f66c7e235d781abcd525e4
vinay-ebi <vinay@ebi.ac.uk>
2022-05-03 14:20:44 +0100

    copy right year changed

--- modules/Bio/EnsEMBL/PipeConfig/BuildVariationMartMTMPTables_conf.pm
+++ modules/Bio/EnsEMBL/PipeConfig/BuildVariationMartMTMPTables_conf.pm
@@ -6,7 +6,7 @@
 =head1 DESCRIPTION  
 
 =head1 LICENSE
-    Copyright [1999-2021] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+    Copyright [1999-2022] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
     Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
          http://www.apache.org/licenses/LICENSE-2.0
